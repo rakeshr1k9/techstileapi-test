3d8849c1c6d41265ed554dea7ca2c036a1ed31c6
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-03-03 11:22:14 +0530

    initial git

---
+++ src/main/java/com/ogmatech/api/techstileapitest/TechstileapiTestApplication.java
@@ -0,0 +1,12 @@
+package com.ogmatech.api.techstileapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstileapiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstileapiTestApplication.class, args);
+	}
+}
